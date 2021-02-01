46e5e7ac370a8bc895895b0e90954d6514b32aed
Bogdan Poplauschi <bpoplauschi@gmail.com>
2021-02-01 12:56:14 +0200

    Ran pod install again on the DemoProjec target and also removed the OTHER_LDFLAGS "-framework", "\"ApolloCommonsAgentInfo\"", "-framework", "\"ApolloCommonsJSONParsing\"", "-framework", "\"Foundation\"", "-framework", "\"Mantle\"",

---
+++ Pods/ApolloCommonsAgentInfo/ApolloCommonsAgentInfo.framework/Headers/TestModelManager.h
@@ -0,0 +1,17 @@
+//
+//  TestModelManager.h
+//  ApolloCommonsAgentInfo
+//
+//  Created by RN on 01/02/21.
+//  Copyright © 2021 Better World Technologies Private Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestModelManager : NSObject
+-(void)manageSomething;
+@end
+
+NS_ASSUME_NONNULL_END
